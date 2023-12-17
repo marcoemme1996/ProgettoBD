e1cc24d42944df8d196ddd55454230ba3d44ddb5
marcoemme1996 <152794134+marcoemme1996@users.noreply.github.com>
2023-12-17 17:09:44 +0100

    ProgettoBD

---
+++ Database/Inserimenti/popolazione_azienda.sql
@@ -0,0 +1,5 @@
+INSERT INTO az.Azienda (CodAzienda, Nome, Sede) VALUES (1, 'Apple', 'Napoli');
+INSERT INTO az.Azienda (CodAzienda, Nome, Sede) VALUES (2, 'Microsoft', 'Roma');
+INSERT INTO az.Azienda (CodAzienda, Nome, Sede) VALUES (3, 'Amazon', 'Milano');
+INSERT INTO az.Azienda (CodAzienda, Nome, Sede) VALUES (4, 'Mastercard', 'Firenze');
+INSERT INTO az.Azienda (CodAzienda, Nome, Sede) VALUES (5, 'Adobe', 'Venezia');	
